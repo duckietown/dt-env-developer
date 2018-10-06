15e0c918c224c91caf1117825a3e4c48f3a09550
Andrea Censi <acensi@ethz.ch>
2018-10-06 21:29:30 +0200

    fix

--- containers/rpi-3-software-main/refresh.sh
+++ containers/rpi-3-software-main/refresh.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-t=build-no-cache
+t=build 
 
 make -C rpi-ros-kinetic-base $t
 
@@ -24,20 +24,6 @@ dockviz images --tree duckietown/rpi-ros-kinetic-base:master18
 
 echo "if you press I will also push"
 read
-
-images=(
-    duckietown/rpi-ros-kinetic-base:master18
-    duckietown/rpi-ros-kinetic-roscore:master18
-    duckietown/rpi-duckiebot-base:master18
-    duckietown/rpi-duckiebot-joystick-demo:master18
-    duckietown/rpi-duckiebot-lanefollowing-demo:master18
-    duckietown/rpi-duckiebot-ros-picam:master18
-    duckietown/rpi-duckiebot-camera-node:master18
-    duckietown/rpi-duckiebot-web_video_server:master18  )
-
-for image in $images; do
-    docker push $image
-done
 
 #make -C rpi-duckiebot-prettylights build-no-cache
 
