49b44625a3453cddccc5b86066754f5b8bc29c95
Davide Iafrate <dvde.iafrate98@gmail.com>
2025-01-20 18:35:10 +0000

    Added mr checkout of sub-repositories

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,3 +1,6 @@
-catkin_make --pkg duckietown_msgs --source /ente/robot/dt-ros-commons/packages --build /home/vscode/build
-direnv allow /ente
-source /ente/devel/setup.bash
+direnv allow /dt-env-developer
+make mrtrust-all
+mr checkout
+# Build and install the dt-ros-commons packages
+catkin_make --pkg duckietown_msgs --source /dt-env-developer/robot/dt-ros-commons/packages --build /home/vscode/build
+source /dt-env-developer/devel/setup.bash
