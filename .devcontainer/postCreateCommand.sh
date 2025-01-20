9b738059c00e32dfbb8a66ae5a3cd93617fb4980
Davide Iafrate <dvde.iafrate98@gmail.com>
2025-01-20 19:08:06 +0000

    Fix: sourcing of ros duckietown_msgs package

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,6 @@
 direnv allow /dt-env-developer
 make mrtrust-all
 mr checkout
-# Build and install the dt-ros-commons packages
+# Build and install the duckietown_msgs packages
 catkin_make --pkg duckietown_msgs --source /dt-env-developer/robot/dt-ros-commons/packages --build /home/vscode/build
-source /dt-env-developer/devel/setup.bash
+. /dt-env-developer/devel/setup.bash
