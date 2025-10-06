a64e08b00beb8be1271730dbda855d970edf59a9
Davide iafrate <davide.iafrate@duckietown.com>
2025-10-06 14:20:22 +0000

    fix: remove unnecessary dbus directory creation in postStartCommand script

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -4,8 +4,6 @@
 pipx install duckietown-shell
 direnv allow /workspaces/dt-env-developer/.envrc
 
-# Create dbus directory
-sudo mkdir -p /var/run/dbus
 
 # Start dbus daemon
 sudo dbus-daemon --system --fork
