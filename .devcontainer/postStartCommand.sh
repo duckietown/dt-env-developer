923b3773d76f5b12ad286b969f1f4baca8fea7c2
Davide iafrate <davide.iafrate@duckietown.com>
2025-10-13 15:28:05 +0000

    fix: virtual duckiebot mDNS domain resolution takes ages

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Configure mDNS for fast .local resolution
+sudo sed -i 's/^hosts:.*/hosts: files mdns4_minimal [SUCCESS=return] mdns6_minimal [SUCCESS=return] dns/' /etc/nsswitch.conf
+sudo grep -q "single-request-reopen" /etc/resolv.conf || sudo echo "options timeout:1 attempts:1 single-request-reopen" | sudo tee -a /etc/resolv.conf
 # Install duckietown-shell
 pipx install duckietown-shell
 
@@ -14,6 +17,8 @@ export DOCKER_CONFIG="$(mktemp -d)"
 printf '{}' > "$DOCKER_CONFIG/config.json"
 
 # Add DOCKER_CONFIG to ~/.bashrc for persistent access
-echo "export DOCKER_CONFIG=\"$DOCKER_CONFIG\"" >> ~/.bashrc
+if ! grep -q "export DOCKER_CONFIG=" ~/.bashrc; then
+	echo "export DOCKER_CONFIG=\"$DOCKER_CONFIG\"" >> ~/.bashrc
+fi
 
 echo "Setup complete. Please open a new terminal."
